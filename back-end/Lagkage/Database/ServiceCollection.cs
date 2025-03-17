b4222ba8466f6333fa0792e5665eb7a4ff0fb041
Søren Frandsen <sfrandsen8@gmail.com>
2025-03-17 20:14:58 +0100

    Init branch (#2)
    * added solution with webapi, dbUp and tests of the first repository
    
    * Removed DotSettings.user from version control
    
    * Stop tracking appsetting.Development.json
    
    * removed minimal api project
    
    * removed template code
    
    * change lagkage folder to backend

---
+++ back-end/Lagkage/Database/ServiceCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace Lagkage.Database;
+
+public static class DBServiceCollectionExtensions
+{
+   public static IServiceCollection AddRepositories(this IServiceCollection services)
+   {
+      Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+      return services.AddScoped<CakeLayerRepository>();
+   }
+}
+
