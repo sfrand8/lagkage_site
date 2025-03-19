145913c48dafe23e190eea3e569f1b67bd9eefdc
Soren <sfrandsen8@gmail.com>
2025-03-19 16:22:22 +0100

    restructured db context into repositories service collection extension method

--- back-end/Lagkage.Database/ServiceCollection.cs
+++ back-end/Lagkage.Database/ServiceCollection.cs
@@ -1,5 +1,8 @@
-﻿using Lagkage.Contracts.Interfaces;
+﻿using System.Data;
+using Lagkage.Contracts.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 namespace Lagkage.Database;
 
@@ -7,6 +10,11 @@ public static class DBServiceCollectionExtensions
 {
    public static IServiceCollection AddRepositories(this IServiceCollection services)
    {
+      services.AddScoped<IDbConnection>(sp =>
+      {
+         var config = sp.GetRequiredService<IConfiguration>();
+         return new NpgsqlConnection(config.GetConnectionString("DefaultConnection"));
+      });
       Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
       return services.AddScoped<ICakeLayerRepository, CakeLayerRepository>();
