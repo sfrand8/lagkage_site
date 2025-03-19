9c12413138a027917c746336b8038634fe0c9fb2
Søren Frandsen <sfrandsen8@gmail.com>
2025-03-19 16:22:44 +0100

    restructured db context into repositories service collection extension method (#5)

--- back-end/Lagkage.API/Program.cs
+++ back-end/Lagkage.API/Program.cs
@@ -7,12 +7,7 @@ internal class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddScoped<IDbConnection>(sp =>
-        {
-            var config = sp.GetRequiredService<IConfiguration>();
-            return new NpgsqlConnection(config.GetConnectionString("DefaultConnection"));
-        });
+        
         builder.Services.AddRepositories();
         builder.Services.AddControllers();
 
