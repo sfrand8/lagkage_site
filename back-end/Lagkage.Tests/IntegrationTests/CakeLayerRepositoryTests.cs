54ab17460c17eb91f5d54120ae88610176b0f2e6
Soren <sfrandsen8@gmail.com>
2025-03-22 15:31:51 +0100

    added more repo tests

--- back-end/Lagkage.Tests/IntegrationTests/CakeLayerRepositoryTests.cs
+++ back-end/Lagkage.Tests/IntegrationTests/CakeLayerRepositoryTests.cs
@@ -15,7 +15,7 @@ public class CakeLayerRepositoryTests
     }
 
     [Fact]
-    public async Task ReturnEmptyListWhenNoneSaved()
+    public async Task GetCakeLayers_ReturnEmptyListWhenNoneSaved()
     {
         _databaseFixture.TruncateAllTables();
         var sut = new CakeLayerRepository(_databaseFixture.Connection);
@@ -25,9 +25,8 @@ public class CakeLayerRepositoryTests
         Assert.Empty(layers);
     }
 
-    [Theory]
-    [AutoData]
-    public async Task ReturnSavedCakeLayersWhenSaved(CakeLayer cakeLayer)
+    [Theory, AutoData]
+    public async Task GetCakeLayers_ReturnSavedCakeLayersWhenSaved(CakeLayer cakeLayer)
     {
         var sut = new CakeLayerRepository(_databaseFixture.Connection);
         
@@ -35,5 +34,59 @@ public class CakeLayerRepositoryTests
         var layer = await sut.GetCakeLayerById(cakeLayer.Id);
         
         Assert.True(cakeLayer.Equals(layer));
+    }
+    
+    [Theory, AutoData]
+    public async Task GetCakeLayerById_ReturnNullWhenNotSaved(CakeLayerId id)
+    {
+        var sut = new CakeLayerRepository(_databaseFixture.Connection);
+        
+        var layer = await sut.GetCakeLayerById(id);
+        
+        Assert.Null(layer);
+    }
+    
+    [Theory, AutoData]
+    public async Task GetCakeLayerById_ReturnSavedCakeLayerWhenSaved(CakeLayer cakeLayer)
+    {
+        var sut = new CakeLayerRepository(_databaseFixture.Connection);
+        
+        await sut.AddCakeLayer(cakeLayer);
+        var layer = await sut.GetCakeLayerById(cakeLayer.Id);
+        
+        Assert.True(cakeLayer.Equals(layer));
+    }
+    
+    [Theory, AutoData]
+    public async Task AddCakeLayer_ReturnOneWhenAddingNew(CakeLayer cakeLayer)
+    {
+        var sut = new CakeLayerRepository(_databaseFixture.Connection);
+        
+        var result = await sut.AddCakeLayer(cakeLayer);
+        
+        Assert.Equal(1, result);
+    }
+    
+    [Theory, AutoData]
+    public async Task AddCakeLayer_ReturnZeroWhenAddingDuplicate(CakeLayer cakeLayer)
+    {
+        var sut = new CakeLayerRepository(_databaseFixture.Connection);
+        
+        await sut.AddCakeLayer(cakeLayer);
+        var result = await sut.AddCakeLayer(cakeLayer);
+        
+        Assert.Equal(0, result);
+    }
+    
+    [Theory, AutoData]
+    public async Task DeleteLayer(CakeLayer cakeLayer)
+    {
+        var sut = new CakeLayerRepository(_databaseFixture.Connection);
+        
+        await sut.AddCakeLayer(cakeLayer);
+        await sut.DeleteCakeLayer(cakeLayer.Id);
+        var layer = await sut.GetCakeLayerById(cakeLayer.Id);
+        
+        Assert.Null(layer);
     }
 }
